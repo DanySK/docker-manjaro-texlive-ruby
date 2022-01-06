e8447c60edec332efcf87618630b7e888c17aa28
Danilo Pianini <danilo.pianini@gmail.com>
2022-01-06 21:21:35 +0100

    chore(release): compact syntax in semantic-release config file

--- release.config.js
+++ release.config.js
@@ -3,15 +3,14 @@ IMAGE_NAME="danysk/docker-manjaro-texlive-ruby"
 docker build -t "$IMAGE_NAME:\${nextRelease.version}"
 docker push --all-tags "$IMAGE_NAME"
 `
-var exec = [
-    "@semantic-release/exec",
-    {
-        "publishCmd": publishCmd,
-    }
-]
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    exec,
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
     "@semantic-release/github",
     "@semantic-release/git",
 )
