aa815204edf16ed04ac82cc36f332c7580efdcb7
Danilo Pianini <danilo.pianini@unibo.it>
2022-02-21 13:59:38 +0100

    fix(release): add missing argument to docker build

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 var publishCmd = `
 IMAGE_NAME="danysk/docker-manjaro-texlive-ruby"
-docker build -t "$IMAGE_NAME:\${nextRelease.version}"
+docker build -t "$IMAGE_NAME:\${nextRelease.version}" .
 docker push --all-tags "$IMAGE_NAME"
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
